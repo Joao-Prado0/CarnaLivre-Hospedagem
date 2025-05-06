4dc2b779611fe45a7d7d5ce4fbb2cbd97966af6b
Joao Prado <joaopradocampos@gmail.com>
2025-05-06 18:30:03 -0300

    Alteração na criação dos cards dinâmicos + alteração em formataçaõ + adicionar mais blocos no JSON fictício

--- codigo/JoaoPrado/assets/js/pesquisa.js
+++ codigo/JoaoPrado/assets/js/pesquisa.js
@@ -1,24 +1,45 @@
 $(document).ready(function(){
-    let blocosCarregados = 6;
+    let blocosCarregados = 0;
+    const blocosPagina = 6;
+    let blocos = [];
     $.getJSON('assets/js/dados.json', function(data){
-        let blocos = data.blocos;
-        $('.card-blocos').each(function(index){
-            let cardbloco = blocos[index];
-            if(cardbloco){
-            $(this).find('img').attr('src', cardbloco.imagem);
-            $(this).find('img').attr('alt', cardbloco.nome);
-            $(this).find('h3').text(cardbloco.nome);
-            $(this).find('p').text(cardbloco.descricao_card);
-            $(this).find('a').attr('href', 'bloco.html?id=' + cardbloco.id);
-            }
-        });
-        $('#btn-ver-mais').click(function() {
-            $('.card-blocos:hidden').slice(0, 6).fadeIn(500);
-            blocosCarregados += 6;
-            if (blocosCarregados >= blocos.length) {
-                $(this).hide();
-            }
+        blocos = data.blocos;
+
+        // Carrega os primeiros blocos
+        carregarMaisBlocos();
+    });
+    // Ao clicar no botão "Ver mais"
+    $('#btn-ver-mais').click(function() {
+        carregarMaisBlocos();
+    });
+
+    function carregarMaisBlocos() {
+        const proximoLote = blocos.slice(blocosCarregados, blocosCarregados + blocosPagina);
+        proximoLote.forEach(function(bloco){
+            const card = criarCard(bloco);
+            $('#container-cards').append(card.hide().fadeIn(500));
         });
 
-    })
+        blocosCarregados += blocosPagina;
+
+        if (blocosCarregados >= blocos.length) {
+            $('#btn-ver-mais').hide(); // Esconde o botão se não houver mais blocos
+        }
+    }
 })
+
+function criarCard(cardbloco){;
+    let div = $('<div>').attr('class', 'card-blocos')
+    let img = $('<img>').attr('src', cardbloco.imagem).attr('alt', 'Descrição');
+    let h3 = $('<h3>').text(cardbloco.nome);
+    let p = $('<p>').text(cardbloco.descricao_card); 
+    let i = $('<i>').html(`<i class="fa-solid fa-star"></i> ${cardbloco.avaliacao}`);
+    let link = $('<a>').attr('href','bloco.html?id=' + cardbloco.id).attr('id', 'linkBloco');
+    let button = $('<button>').text('Ver mais');
+
+    link.append(button);
+
+    div.append(img,i,h3,p,link);
+    return div;
+}
+
