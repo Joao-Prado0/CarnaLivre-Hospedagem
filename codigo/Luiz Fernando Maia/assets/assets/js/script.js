c89f44559e4c978c1a9f23a460e664135db964fc
LuizMaia <lf.cunhamaia@gmail.com>
2025-05-07 12:53:39 -0300

    altereções final no html e js

--- codigo/Luiz Fernando Maia/assets/assets/js/script.js
+++ codigo/Luiz Fernando Maia/assets/assets/js/script.js
@@ -10,3 +10,37 @@ function togglePassword() {
     eyeIcon.src = "assets/icon/eye-icon.svg" ; 
   }
 }
+
+document.querySelector(".login-btn").addEventListener("click", function () {
+  setTimeout(() => {
+    const alertHTML = `
+      
+            <div class="alert alert-warning" role="alert">
+         Você esta sendo redirecionado para pagina Inicial!
+      </div>
+      <div class="alert alert-success" role="alert">
+        Login realizado com sucesso.Caso queira ir para tela de cadastro <a href="codigo/HenriquePereira/cadastrousuario.html" class="alert-link">Clique aqui.</a>.
+      </div>
+
+    `;
+
+
+    document.getElementById("alert-container").innerHTML = alertHTML;
+  }, 1500);
+});
+
+document.querySelector(".forgot").addEventListener("click", function () {
+  setTimeout(() => {
+    const alertHTML = `
+      
+      
+      <div class="alert alert-success" role="alert">
+        Você recebera um e-mail pedindo a confirmação da senha.Caso queira reenviar o e-mail <a href="codigo/HenriquePereira/cadastrousuario.html" class="alert-link">Clique aqui.</a>.
+      </div>
+    `;
+
+    document.getElementById("alert-container").innerHTML = alertHTML;
+  }, 1500);
+});
+
+
