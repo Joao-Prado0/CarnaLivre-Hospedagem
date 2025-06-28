5f7e5c3fa486cb696ce6acd82e75449869cc6618
Joao Prado <joaopradocampos@gmail.com>
2025-06-28 14:31:33 -0300

    Teste Render

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('./data/db.json');
+const router = jsonServer.router({});
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
-server.listen(3000, () => {
-  console.log('JSON Server is running');
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running (in-memory only)');
 });
